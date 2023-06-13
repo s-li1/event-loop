f19178dec06abf0a8a8f48a5115245fa468105f5
s-li1 <stevenli8892@hotmail.com.au>
2023-06-13 13:19:37 +1000

    basic implementation

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <thread>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+
+std::queue<int> eventQueue;
+
+std::mutex mutex;
+std::condition_variable cv;
+
+void processEvent(int event)
+{
+    std::cout << "Processing event: " << event << std::endl;
+}
+
+void eventLoop()
+{
+    while (true)
+    {
+        std::unique_lock<std::mutex> lock(mutex);
+        std::cerr << "Waiting... \n";
+
+        cv.wait(lock, [] { return !eventQueue.empty(); });
+
+        int event = eventQueue.front();
+        eventQueue.pop();
+
+        lock.unlock();
+
+        processEvent(event);
+    }
+}
+
+int main()
+{
+    std::thread loopThread(eventLoop);
+
+    for (int i = 0; i < 10; ++i)
+    {
+        eventQueue.push(i);
+
+        cv.notify_one();
+    }
+
+    loopThread.join();
+
+    return 0;
+}
